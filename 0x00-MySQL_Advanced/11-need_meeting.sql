e6f2099d0c7f729ebc785165c2e979043228ca8f
Ahmed Wael <xahmedwaheed@gmail.com>
2025-01-19 18:55:34 +0000

    elev

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,7 @@
+-- create a meeting view
+CREATE VIEW need_meeting AS
+SELECT name FROM students
+WHERE score < 80
+AND (last_meeting IS NULL 
+OR 
+last_meeting < ADDDATE(CURDATE(), interval -1 MONTH));
