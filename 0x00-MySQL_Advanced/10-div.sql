1c0bf00f191a38d5d8c4516c95ac4ef6e99d1afa
Ahmed Wael <xahmedwaheed@gmail.com>
2025-01-19 18:05:57 +0000

    ten

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,10 @@
+-- creating a function for safe division
+DELIMITER &&
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT DETERMINISTIC
+BEGIN
+	DECLARE result FLOAT;
+	IF b = 0 THEN RETURN 0;
+	ELSE RETURN a / b;
+	END IF;
+END &&
+DELIMITER ;
