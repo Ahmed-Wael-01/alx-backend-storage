792f9cc81f1781232845b5cb9065ec9a271b51cf
Ahmed-Wael-01 <xahmedwaheed@gmail.com>
2025-02-01 08:17:04 -0800

    twelve

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
-"""providing stats about nginx logs"""
-
-
+""" MongoDB Operations with Python using pymongo """
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    """mhmmm"""
+    """ Provides some stats about Nginx logs stored in MongoDB """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    col = client.logs.nginx
-    print(col.count_documents(), 'logs')
+    nginx_collection = client.logs.nginx
+
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print('Methods:')
-    for meth in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        print('\tmethod {}: {}'.format(meth, col.count_documents({"method": meth})))
-    print('{} status check'.format(col.count_documents(
-                                            {"method": "GET",
-                                             "path": "/status"})))
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{status_check} status check')
---
+++ 0x01-NoSQL/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""providing stats about nginx logs"""
+
+
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    """mhmmm"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    col = client.logs.nginx
+    print(col.count_documents(), 'logs')
+    print('Methods:')
+    for meth in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print('\tmethod {}: {}'.format(meth, col.count_documents({"method": meth})))
+    print('{} status check'.format(col.count_documents(
+                                            {"method": "GET",
+                                             "path": "/status"})))
