9dc62d4128a15b9c65f8c07c996d7eb472da361d
Ahmed-Wael-01 <xahmedwaheed@gmail.com>
2025-02-01 08:25:38 -0800

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+"""providing stats about nginx logs"""
+
+
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    """ Provides some stats about Nginx logs stored in MongoDB """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
-
-    n_logs = nginx_collection.count_documents({})
-    print(f'{n_logs} logs')
-
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    """mhmmm"""
+    client = MongoClient()
+    col = client.logs.nginx
+    print(col.count_documents({}), 'logs')
     print('Methods:')
-    for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f'\tmethod {method}: {count}')
-
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-
-    print(f'{status_check} status check')
+    for meth in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print('\tmethod {}: {}'.format(meth, col.count_documents({"method": meth})))
+    print('{} status check'.format(col.count_documents(
+                                            {"method": "GET",
+                                             "path": "/status"})))
