a026a100d07f47cf5164951d31a79ccad6a001fe
Ahmed-Wael-01 <xahmedwaheed@gmail.com>
2025-02-02 10:16:26 -0800

    web tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -10,6 +10,8 @@ cache = redis.Redis()
 def get_page(url: str) -> str:
     """gets a page and track requests"""
     cache.incr('count:{}'.format(url))
+    if cache.get('count:{}'.format(url)):
+        return cache.get('count:{}'.format(url))
     req = requests.get(url)
     cache.setex('count:{}'.format(url), 10, req.text)
     return req.text
