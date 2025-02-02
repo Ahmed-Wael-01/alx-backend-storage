615692ccc729f823accdfca486ab0f3fded5cad5
Ahmed-Wael-01 <xahmedwaheed@gmail.com>
2025-02-02 10:12:08 -0800

    web tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""web cache and tracker"""
+import redis
+import requests
+
+
+cache = redis.Redis()
+
+
+def get_page(url: str) -> str:
+    """gets a page and track requests"""
+    cache.incr('count:{}'.format(url))
+    req = requests.get(url)
+    cache.setex('count:{}'.format(url), 10, req.text)
+    return req.text
